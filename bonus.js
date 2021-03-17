1f6d07a76055c37418696012eb5f6978098a1f6f
Tejaswi Nallavolu <tejaswireddynallavolu@gmail.com>
2021-03-17 15:13:59 -0500

    bonus

---
+++ bonus.js
@@ -0,0 +1,43 @@
+const http = require('http')    //Pull in a useful node package
+
+
+const hostname = process.env.hostname || '127.0.0.1'    //get our ip address from the environment
+const port = process.env.port || 3001               //and the port
+
+const server =
+    http.createServer(            //Creates the response loop
+        (req, res) => {               //Anonymous function to handle the request
+            console.log("Request recieved")
+            // console.log(req.url)
+            // Build a fake url so we can get the search parameters using a URL object
+            fake_url = "https://fake.com/path" + req.url
+            const url = new URL(fake_url)
+            if (req.method === 'GET') {
+
+                //   console.log("Look for query parameter data: " + search_params.get("data"))
+                const Webseries = ["Bates Motel",
+                "How to get away with murder",
+                "little things",
+                "13 reasons why",
+                "breathe into shadows",
+                "stranger things",
+                ]
+                let randomIndex = Math.floor(Math.random() * Webseries.length)
+                // Process the queries here
+                res.statusCode = 200      //code for OK
+                res.setHeader('Content-Type', 'text/plain')
+                res.write(`${Webseries[randomIndex]}`)
+                res.end();
+
+            } else {
+                console.log("Status 404")
+                res.statusCode = 404;
+                res.end();
+            }
+
+        }
+    )
+
+server.listen(port, hostname, () => {   //Start the server
+    console.log(`Server running at http://${hostname}:${port}/`)  //Log the request
+})
